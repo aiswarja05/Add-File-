bacd30608de6800c2c87f471a77a8e62d050fc80
Aiswarja Saha Joye <48473395+aiswarja05@users.noreply.github.com>
2019-03-14 11:43:53 +0600

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,12 @@
+package CRUD;
+
+public interface Information {
+		public String add(String n, String d, String o, String s);
+		public String Remove();
+		public String Search();
+		public String Update();
+		
+		
+		
+
+	}
